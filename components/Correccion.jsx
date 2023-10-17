113a09fda9339feece910e1a677f64e35c179b92
Andrea <ands.sanchez00@gmail.com>
2023-10-17 12:41:51 -0600

    Se agregó la tarjeta de corrección y se agregaron nuevos estilos a css

--- components/Correccion.jsx
+++ components/Correccion.jsx
@@ -6,27 +6,26 @@ export const Correccion = () => {
     return (
         <div className="container">
             <div>
-                <h1>Correccion</h1>
+                <h1>Corrección</h1>
             </div>
             <div className="container">
                 <div className="row">
                     <div className="col">
-
-                    </div>
-                    <div className="col-lg">
-                        <h3>Ingresa tu texto aquí </h3>
-
-                        <input
+                        <h3>Ingresa tu texto aquí: </h3>
+                        <textarea
                             className="form-control form-control-lg"
-                            type="text"
+                            id="text-input"
+                            rows="6" // El número de filas que se mostrarán, ajusta según tus necesidades
+                            style={{ resize: 'none' }} // Evita que el usuario pueda redimensionar el área de texto
                         />
-
-
                     </div>
                     <div className="col">
+                        <div className='corrected-text'>
+                        </div>
 
                     </div>
                 </div>
+
             </div>
         </div>
     )
